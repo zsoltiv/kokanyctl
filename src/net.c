a6da84dc6a7d4c6895a30f11e2c3f0542616a528
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2023-04-17 19:20:52 +0200

    net: Add `net_ffmpeg_format_url`
    This function generates a URL used in `avformat_open_input()` from an
    SDLNet `IPaddress`

--- src/net.c
+++ src/net.c
@@ -11,7 +11,10 @@
 #include "net.h"
 
 #define NET_INTERFACE "wlan0"
-#define NET_REMOTE "192.168.0.1"
+#define NET_PORT 1337u
+#define NET_FFMPEG_PROTO "tcp://"
+#define NET_FFMPEG_OPTS "?listen=1"
+#define NET_REMOTE "127.0.0.1"
 
 void net_get_local_address(IPaddress *ip)
 {
@@ -33,4 +36,30 @@ void net_get_local_address(IPaddress *ip)
 void net_resolve_kokanybot(IPaddress *ip)
 {
     SDLNet_ResolveHost(ip, NET_REMOTE, 0);
+}
+
+const char *net_ffmpeg_format_url(IPaddress *ip)
+{
+    const char *ip_string = SDLNet_ResolveIP(ip);
+    if(!ip_string)
+        return NULL;
+
+    char port[13] = {0};
+    snprintf(port, sizeof(port), ":%u", NET_PORT);
+
+    size_t port_sz = strlen(port);
+    size_t ip_sz = strlen(ip_string);
+    size_t ffmpeg_opts_sz = strlen(NET_FFMPEG_OPTS);
+    size_t ffmpeg_proto_sz = strlen(NET_FFMPEG_PROTO);
+    size_t total = ffmpeg_proto_sz + ip_sz + port_sz + ffmpeg_opts_sz + 1;
+    char *buf = calloc(total + 1, 1);
+    snprintf(buf,
+             total,
+             "%s%s:%s%s",
+             NET_FFMPEG_PROTO,
+             ip_string,
+             port,
+             NET_FFMPEG_OPTS);
+
+    return buf;
 }
--- src/net.h
+++ src/net.h
@@ -5,5 +5,6 @@
 
 void net_get_local_address(IPaddress *ip);
 void net_resolve_kokanybot(IPaddress *ip);
+const char *net_ffmpeg_format_url(IPaddress *ip);
 
 #endif /* NET_H */
