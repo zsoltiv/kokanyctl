95d3f72f663525c70ce99be4f6532e19c66dced1
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2023-04-20 19:44:15 +0200

    net: Move function and enable ethernet access
    Moved `encode_scancode` to `net` (`net_encode_scancode`) and added
    NET_ETHERNET. Also renamed NET_INTERFACE to NET_WIRELESS.

--- src/net.c
+++ src/net.c
@@ -1,4 +1,5 @@
 #include <ifaddrs.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <netinet/in.h>
@@ -10,11 +11,12 @@
 
 #include "net.h"
 
-#define NET_INTERFACE "wlan0"
+#define NET_WIRELESS "wlan0"
+#define NET_ETHERNET "eth0"
 #define NET_PORT 1337u
 #define NET_FFMPEG_PROTO "tcp://"
 #define NET_FFMPEG_OPTS "?listen=1"
-#define NET_REMOTE "127.0.0.1"
+#define NET_REMOTE "192.168.0.99"
 
 void net_get_local_address(IPaddress *ip)
 {
@@ -22,12 +24,15 @@ void net_get_local_address(IPaddress *ip)
     getifaddrs(&interfaces);
     ifap = interfaces;
     do {
-        if(!strcmp(NET_INTERFACE, ifap->ifa_name) &&
+        if((!strcmp(NET_WIRELESS, ifap->ifa_name) ||
+            !strcmp(NET_ETHERNET, ifap->ifa_name)) &&
            ifap->ifa_addr->sa_family == AF_INET)
             break;
     } while((ifap = ifap->ifa_next));
     if(!ifap)
-        ctl_die("net_get_local_address(): no interface named %s\n", NET_INTERFACE);
+        ctl_die("net_get_local_address(): no interfaces named %s and %s\n",
+                NET_WIRELESS,
+                NET_ETHERNET);
     struct sockaddr_in *inaddr = (struct sockaddr_in *)ifap->ifa_addr;
     ip->host = inaddr->sin_addr.s_addr;
     freeifaddrs(interfaces);
@@ -36,6 +41,11 @@ void net_get_local_address(IPaddress *ip)
 void net_resolve_kokanybot(IPaddress *ip)
 {
     SDLNet_ResolveHost(ip, NET_REMOTE, 0);
+}
+
+uint8_t net_encode_scancode(uint8_t scancode, bool pressed)
+{
+    return (uint8_t)SDL_GetKeyFromScancode(scancode) | (uint8_t)(pressed << 7);
 }
 
 const char *net_ffmpeg_format_url(IPaddress *ip)
--- src/net.h
+++ src/net.h
@@ -1,10 +1,14 @@
 #ifndef NET_H
 #define NET_H
 
+#include <stdint.h>
+#include <stdbool.h>
+
 #include "SDL_net.h"
 
 void net_get_local_address(IPaddress *ip);
 void net_resolve_kokanybot(IPaddress *ip);
+uint8_t net_encode_scancode(uint8_t scancode, bool pressed);
 const char *net_ffmpeg_format_url(IPaddress *ip);
 
 #endif /* NET_H */
