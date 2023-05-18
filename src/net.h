71367d54de1019576eb1f49c652009e397235d5a
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2023-05-18 16:27:29 +0200

    net: Add `extern "C"` to header

--- src/net.h
+++ src/net.h
@@ -1,6 +1,10 @@
 #ifndef NET_H
 #define NET_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdint.h>
 #include <stdbool.h>
 
@@ -10,5 +14,9 @@ TCPsocket net_connect_to_remote(void);
 uint8_t net_encode_scancode(uint8_t scancode, bool pressed);
 void net_send_keycode(TCPsocket remote, uint8_t keycode);
 const char *net_ffmpeg_format_url(IPaddress *ip);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* NET_H */
