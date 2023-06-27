52665b65b32d704129e40c157aa289a40bbafb1d
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2023-06-27 22:29:00 +0200

    kokanyrecognize: Add code for image recognition using YOLOv5

--- kokanyrecognize.py
+++ kokanyrecognize.py
@@ -24,7 +24,33 @@ def draw_motion(still, current):
         current[rows[i], cols[i]] = MOTION_COLOR
 
 
+IMGSZ = 640
 url = 'tcp://' + argv[-1] + ':1338'
+model = cv.dnn.readNet('yolo/model.onnx')
+model.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+model.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+
+
+def draw_bounding_box(frame, detection):
+    x, y, w, h = detection[0], detection[1], detection[2], detection[3]
+    WIDTH_SCALE = frame.shape[0] / IMGSZ
+    HEIGHT_SCALE = frame.shape[1] / IMGSZ
+    x = int(x * WIDTH_SCALE)
+    w = int(abs(x - w) / 2 * WIDTH_SCALE)
+    y = int(y * HEIGHT_SCALE)
+    h = int(abs(y - h) / 2 * HEIGHT_SCALE)
+    cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+
+def feed_model(frame):
+    CONFIDENCE_THRESHOLD = 0.5
+    foo = frame.copy()
+    blob = cv.dnn.blobFromImage(foo, 1 / 255.0, (IMGSZ, IMGSZ), swapRB=False, crop=False)
+    model.setInput(blob)
+    predictions = model.forward()[0]
+    confident = np.extract(predictions[:,4] > CONFIDENCE_THRESHOLD, predictions)
+    draw_bounding_box(frame, predictions[0])
+
 
 cap = cv.VideoCapture(url)
 if not cap.isOpened():
@@ -41,6 +67,7 @@ while True:
     if ret is False:
         continue
     draw_motion(still, frame)
+    feed_model(frame)
     if still is not None:
         cv.imshow('motion', frame)
         cv.waitKey(1)
