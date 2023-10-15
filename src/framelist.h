a4394f071baf8a478823542aca4072d990c40339
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2023-10-15 17:23:23 +0200

    framelist: Implement circular buffer for storing frames
    This is a circular buffer for storing `AVFrame`s, the motivation is
    decreasing the amount of time threads wait for each other.

---
+++ src/framelist.h
@@ -0,0 +1,20 @@
+#ifndef FRAMELIST_H
+#define FRAMELIST_H
+
+#include <libavutil/frame.h>
+#include "SDL.h"
+
+#include <stdbool.h>
+
+struct frame {
+    AVFrame *avf;
+    SDL_mutex *lock;
+    struct frame *next;
+    bool ready; // consumers check this before using `avf`
+};
+
+struct frame *frame_list_new(const int elems);
+struct frame *frame_list_lock_next(struct frame *current);
+void frame_list_unlock_frame(struct frame *f, bool isready);
+
+#endif
